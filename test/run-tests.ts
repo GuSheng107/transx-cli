68a58541222dc5342ce3bf48b601952716dc4d9b
GuSheng <73179914+GuSheng107@users.noreply.github.com>
2026-08-04 01:25:33 +0800

    chore: remove unused test directory

--- test/run-tests.ts
+++
@@ -1,7 +0,0 @@
-import "./config.test.js";
-import "./paths.test.js";
-import "./translator.test.js";
-import "./ui.test.js";
-import "./languages.test.js";
-import "./history.test.js";
-import "./file-reader.test.js";
